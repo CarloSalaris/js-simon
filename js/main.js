d24b76ff7fea0b521a799ffa58d66cd08b6701b3
CarloSalaris <carloa.salaris@gmail.com>
2023-05-11 20:58:49 +0200

    generazione array numeri indovinati

--- js/main.js
+++ js/main.js
@@ -12,9 +12,14 @@
 
 /* SVOLGIMENTO */
 
+// Outputs
 const outputRandomNum = document.getElementById("outputRandomNum");
 const outputCountdown = document.getElementById("countdown");
+// Arrays
 const randomNumArr = [];
+const InputArr =[];
+const rightNumArr = [];
+
 
 // Generazione 5 numeri casuali (stabiliamo che siano numeri interi compresi tra 1 e 100)
 for (let i = 0; randomNumArr.length < 5; i++) {
@@ -29,17 +34,21 @@ outputRandomNum.innerHTML = randomNumArr;
 console.log(randomNumArr);
 
 //Timer 30 secondi (setInterval perché voglio far vedere il tempo che manca nel countdown)
-let countdownCounter = 30;
+let countdownCounter = 3;
 const countdown = setInterval(function(){
     outputCountdown.innerHTML = countdownCounter;
     countdownCounter--
     if (countdownCounter < 0) {
-        outputRandomNum.innerHTML = "TEMPO SCADUTO! Vediamo cosa ti ricordi";
+        outputRandomNum.innerHTML = "TEMPO SCADUTO! Vediamo cosa ti ricordi...";
         console.log("TEMPO SCADUTO! Vediamo cosa ti ricordi...");
         clearInterval(countdown);
         InputArrFunc(5);
-    }
+        commonElemArrFunc(randomNumArr, InputArr);      
+        }
 }, 1000);
+
+
+
 
 
 
@@ -47,11 +56,22 @@ const countdown = setInterval(function(){
 
 // Generazione array di un numero stabilito di inputs
 function InputArrFunc(howManyInputs) {
-    const InputArr =[];
     for (let i = 0; i < howManyInputs; i++) {
        let inputElement = parseInt(prompt("inserisci un numero: "))
        InputArr.push(inputElement);
     }
     console.log(InputArr);
     return InputArr;
-}
+};
+
+// Generazione array di elementi comuni tra 2 arrays stabiliti
+function commonElemArrFunc(arr1, arr2) {
+    // Confronto dei due arrays. Se ci sono elementi in comune li metto in un nuovo array.
+    for (let i = 0; i < arr2.length; i++) {
+        if (arr1.includes(arr2[i])) {
+            rightNumArr.push(arr2[i]);
+        }
+    }
+    console.log(rightNumArr);
+    return rightNumArr
+};
